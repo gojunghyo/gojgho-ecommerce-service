8ad17ad64e2536e969268a9db36c35e431ed69cc
ITO_MZ_GOJGHO\mz_gojgho <gojgho@mz.co.kr>
2023-12-21 09:56:25 +0900

    init commit

---
+++ src/main/java/com/gojgho/gojghoecommerceservice/GojghoEcommerceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gojgho.gojghoecommerceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GojghoEcommerceServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GojghoEcommerceServiceApplication.class, args);
+  }
+
+}
