c8b49302ede137f329b695f1cf21e64d3401f6d4
ITO_MZ_GOJGHO\mz_gojgho <gojgho@mz.co.kr>
2024-01-04 10:41:49 +0900

    [gojgho-ecommerce-service] active default local 적용

--- service-api/src/main/java/com/gojgho/serviceapi/ecommerce/config/ApiConfig.java
+++ service-api/src/main/java/com/gojgho/serviceapi/ecommerce/config/ApiConfig.java
@@ -4,7 +4,7 @@ import com.gojgho.servicedomain.config.DomainConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-//domain 클래스에 db 정보와 cache 등을 가져옴
+//domain 클래스에 bean, componet 등을 가져옴
 @Configuration
 @Import(DomainConfig.class) //import 어노테이션으로 인해서 datasourceJpaConfig 클래스 등을 가져올수있음
 public class ApiConfig {
